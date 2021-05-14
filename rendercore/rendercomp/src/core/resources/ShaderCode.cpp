f5bbb73e8325159ecc65a66ba671f261d3bccf50
Nadir Roman Guerrero <nadir.ro.gue@gmail.com>
2021-05-14 10:07:06 +0200

    Support for custom #include clause on shaders

--- rendercore/rendercomp/src/core/resources/ShaderCode.cpp
+++ rendercore/rendercomp/src/core/resources/ShaderCode.cpp
@@ -19,6 +19,8 @@
 
 #include <fstream>
 
+#define MAX_SHADER_INCLUDE_DEPTH 64
+
 namespace rendercomp
 {
 namespace data
@@ -53,29 +55,61 @@ String ShaderCode::parseUBERCode(const Vector<String> &defines)
 
 }
 
+std::string LoadShaderCode(const std::string& path, uint32_t depth = 0)
+{
+    if(depth > MAX_SHADER_INCLUDE_DEPTH)
+        throw std::runtime_error("ShaderCode reader: Max include depth ("
+                                 + std::to_string(MAX_SHADER_INCLUDE_DEPTH) + " reached");
+    std::ifstream file (path);
+    if(!file)
+        throw std::runtime_error("ShaderCode reader: Could not open the file " + path);
+
+    const std::string basePath =
+            std::string(FilesystemUtils::getParentDirectory(String(path.c_str())).c_str());
+
+    std::string rawCode;
+    try
+    {
+        std::string buf;
+        size_t line = 0;
+        while(std::getline(file, buf))
+        {
+            if(buf.find("#include") == 0)
+            {
+                const std::string::size_type spacePos = buf.find(' ');
+                if(spacePos == std::string::npos)
+                    throw std::runtime_error("Cannot find space separator on #include clause "
+                                             "on line " + std::to_string(line));
+
+                std::string shaderIncludePath = basePath + buf.substr(spacePos + 1);
+                if(!FilesystemUtils::exists(String(shaderIncludePath.c_str())))
+                    throw std::runtime_error("ShaderCode reader: Cannot find shader from #include "
+                                             "clause \n\t" + buf + "\non path \n\t"
+                                             + shaderIncludePath);
+
+                buf = LoadShaderCode(shaderIncludePath, depth + 1);
+            }
+
+            rawCode += buf + "\n";
+        }
+        //code =  String( std::string(std::istreambuf_iterator<char>(file),
+        //                std::istreambuf_iterator<char>()).c_str() );
+    }
+    catch(const std::exception& e)
+    {
+        throw std::runtime_error("ShaderCode reader: Could not read the file " + path + ": "
+                                 + std::string(e.what()));
+    }
+
+    return rawCode;
+}
+
 template<>
 std::unique_ptr<data::ShaderCode>
 Resources::get<data::ShaderCode>(const String& path)
 {
-
-    const std::string stdPath = TOSTDSTR(path);
-    std::ifstream file (stdPath);
-    if(!file)
-        throw std::runtime_error("ShaderCode reader: Could not open the file " + stdPath);
-
-    try
-    {
-        return std::make_unique<data::ShaderCode>(
-                    String( std::string(std::istreambuf_iterator<char>(file),
-                            std::istreambuf_iterator<char>()).c_str() ));
-    }
-    catch(const std::exception& e)
-    {
-        throw std::runtime_error("ShaderCode reader: Could not read the file " + stdPath + ": "
-                                 + std::string(e.what()));
-    }
-
-    return {nullptr};
+    return std::make_unique<data::ShaderCode>(
+                String(LoadShaderCode(std::string(path.c_str())).c_str()));
 }
 
 }
